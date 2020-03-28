59daed74aca16db9e7aa0f2cd4a4efb3adf6811b
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-03-28 17:44:26 +0100

    New target for CMake

---
+++ algorithms_test.cpp
@@ -0,0 +1,5 @@
+#include "algorithms.hpp"
+
+int main() {
+    return add(3,4);
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "algorithms.hpp"
 
 int main() {
     std::cout << "Hello World\n";
+    const auto result = add(5,6);
+    std::cout << result << '\n';
     return 0;
 }
