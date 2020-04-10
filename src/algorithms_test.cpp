084ea6eae43a42e2013a0701367887cb1a891c54
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-04-10 14:59:15 +0200

    Use catch2 in example

--- src/algorithms_test.cpp
+++ src/algorithms_test.cpp
@@ -1,5 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 #include "algorithms.hpp"
 
-int main() {
-    return add(3,4);
+TEST_CASE("Multiplication works", "[Multiplication]")
+{
+  REQUIRE(mul(1,1) == 1);
+  REQUIRE(mul(2,5) == 10);
+}
+
+TEST_CASE("Addition works", "[Addition]")
+{
+  REQUIRE(add(1,1) == 2);
+  REQUIRE(add(2,5) == 7);
 }
