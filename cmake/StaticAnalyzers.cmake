e299d54b5df480fec303654363466204fe1460e4
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-04-30 22:24:04 +0200

    Make gcc build work again

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -2,7 +2,7 @@
 #Cppcheck
 find_program(CPPCHECK cppcheck)
 if(CPPCHECK)
-set(CMAKE_CXX_CPPCHECK ${CPPCHECK} --enable=all --inconclusive)
+set(CMAKE_CXX_CPPCHECK ${CPPCHECK} --suppress=missingInclude --enable=all --inconclusive)
 else()
 message("cppcheck executable not found")
 endif()
@@ -11,7 +11,7 @@ endif()
 #clang-tidy
 find_program(CLANGTIDY clang-tidy)
 if(CLANGTIDY)
-set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY})
+set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} -extra-arg=-Wno-unknown-warning-option)
 else()
 message("clang-tidy executable not found")
 endif()
