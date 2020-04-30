cfc09ed776e027f05dcdbfac037229a58bd9896b
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-04-30 21:56:02 +0200

    release configs

--- create_build.sh
+++ create_build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-mkdir build-clang
-cd build-clang
+mkdir build-clang-debug
+cd build-clang-debug
 cmake -GNinja -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_CXX_COMPILER:STRING=/usr/bin/clang++ ..
 cd ..
-mkdir build-gcc
-cd build-gcc
+mkdir build-gcc-debug
+cd build-gcc-debug
 cmake -GNinja -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_CXX_COMPILER:STRING=/usr/bin/g++ ..
+cd ..
+mkdir build-gcc-release
+cd build-gcc-release
+cmake -GNinja -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_CXX_COMPILER:STRING=/usr/bin/g++ ..
+cd ..
+mkdir build-clang-release
+cd build-clang-release
+cmake -GNinja -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_CXX_COMPILER:STRING=/usr/bin/clang++ ..
 cd ..
